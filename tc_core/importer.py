cafa4938b360fba3e68119d6fa7c91579739f065
Martin Samson <martin@workshopx.com>
2015-06-15 11:41:49 -0400

    Added license info. Closes #7

--- tc_core/importer.py
+++ tc_core/importer.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2015, thumbor-community
+# Use of this source code is governed by the MIT license that can be
+# found in the LICENSE file.
+
 from thumbor.utils import logger
 from thumbor.importer import Importer as ThumborImporter
 
