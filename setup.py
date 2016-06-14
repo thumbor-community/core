b78271f9022741885a4e2794235d3d09a86d61f6
Martin Samson <martin.samson@shopify.com>
2016-06-14 09:33:59 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='tc_core',
-    version='0.3.0',
+    version='0.4.0',
     url='http://github.com/thumbor_community/core',
     license='MIT',
     author='Thumbor Community',
